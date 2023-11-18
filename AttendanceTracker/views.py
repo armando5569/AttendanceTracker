3f0a3cde68b76aef2fa176156092eabe3bfe4d6d
howardem22 <howardem22@yahoo.com>
2023-11-17 23:00:17 -0600

    started authentication

--- AttendanceTracker/views.py
+++ AttendanceTracker/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 
 
 #app function pages
@@ -12,8 +14,10 @@ def courses(request):
 def editcourse(request):
     return render(request, 'attendancetracker/editcourse.html')
 
+@login_required
 def reports(request):
     return render(request, 'attendancetracker/reports.html')
+
 
 def startclass(request):
     return startclass(request, 'attendancetracker/startclass.html')
