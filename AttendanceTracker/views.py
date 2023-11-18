c61623af2ca1e4e888b5fdbc6c73215c1f18b701
howardem22 <howardem22@yahoo.com>
2023-11-18 15:53:32 -0600

    login required on all pages

--- AttendanceTracker/views.py
+++ AttendanceTracker/views.py
@@ -5,12 +5,15 @@ from django.contrib.auth.decorators import login_required
 
 
 #app function pages
+@login_required
 def confirmation(request):
     return render(request, 'attendancetracker/confirmation.html')
 
+@login_required
 def courses(request):
     return render(request, 'attendancetracker/courses.html')
 
+@login_required
 def editcourse(request):
     return render(request, 'attendancetracker/editcourse.html')
 
@@ -18,6 +21,6 @@ def editcourse(request):
 def reports(request):
     return render(request, 'attendancetracker/reports.html')
 
-
+@login_required
 def startclass(request):
     return startclass(request, 'attendancetracker/startclass.html')
